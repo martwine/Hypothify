dcff10b96446e92e7f92da60cafb0926c2c7ccf6
Martin Johnson <martwine@gmail.com>
2012-03-27 00:25:36 +0100

    top levels  first commit

---
+++ urls.py
@@ -0,0 +1,18 @@
+from hypotheses.views import detail
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'hypothify.views.home', name='home'),
+     url(r'^hypotheses/(?P<hypothesis_id>\d+)/$','hypotheses.views.detail'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+      url(r'^admin/', include(admin.site.urls)),
+)
