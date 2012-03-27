57062d9cdac58dc683cb13008a2ff01a354b3e79
Martin Johnson <martwine@gmail.com>
2012-03-27 14:39:13 +0100

    a much better way of doing ordered item getting

--- hypotheses/views.py
+++ hypotheses/views.py
@@ -5,14 +5,16 @@ from evidences.models import Evidence
 def detail(request,hypothesis_id,**kwargs):
 
 	h=get_object_or_404(Hypothesis, pk=hypothesis_id)
-	#get list of all evidence items by vote
-	e=h.get_top_evidence_items()
-	d=h.get_top_description_items()
-	s=h.get_top_summary_items()
-	#and get the 'best'one, or get the first one if there haven't been any votes yet, or return None if there aren't any at all
-	e1=e[0] if e else h.evidenceset.all()[0] if h.evidenceset.all() else None
-	d1=d[0] if d else h.descriptions.all()[0] if h.descriptions.all() else None
-	s1=s[0] if s else h.summaries.all()[0] if h.summaries.all() else None
+
+	# get associated items ordered by vote 
+	e=h.get_ordered_evidence_items()
+	d=h.get_ordered_description_items()
+	s=h.get_ordered_summary_items()
+	
+	#and get the 'best' / first one or return None if there aren't any at all
+	e1=e[0] if e else None
+	d1=d[0] if d else None
+	s1=s[0] if s else None
 	
 	return render_to_response(
 		'hypothesis_detail.html',
