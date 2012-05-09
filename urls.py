3a73d2e557a7a9cb517c45934973a58ca51da7b4
Martin Johnson <martwine@gmail.com>
2012-05-09 09:22:44 +0100

    generic view urls

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from hypotheses.views import detail
 from django.conf.urls.defaults import *
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 from hypotheses.models import Hypothesis
 
 # Uncomment the next two lines to enable the admin:
@@ -10,8 +10,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', ListView.as_view(model=Hypothesis,)),
-     url(r'^hypotheses/(?P<hypothesis_id>\d+)/$','hypotheses.views.detail'),
-
+     url(r'^hypothesis/(?P<hypothesis_id>\d+)/$','hypotheses.views.detail'),
+     url(r'^hypothesis/add', CreateView.as_view(model=Hypothesis)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
