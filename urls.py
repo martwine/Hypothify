15f0b5a26f2d5ed51f44dfb73782d134cca44fa0
Martin Johnson <martwine@gmail.com>
2012-05-14 21:45:55 +0100

    x

--- urls.py
+++ urls.py
@@ -22,11 +22,14 @@ urlpatterns = patterns('',
      #accounts (login, logout etc)
      (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
      (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+     
      url(r'^hypothesis/add', login_required(HypothesisCreate.as_view(model=Hypothesis))),
      url(r'^hypothesis/(?P<hypothesis_id>\d+)/$',detail),
      url(r'^hypothesis/(?P<hypothesis_id>\d+)/add_evidence/$', login_required(EvidenceCreate.as_view(model=Evidence))),
      url(r'^hypothesis/(?P<hypothesis_id>\d+)/add_description/$', login_required(DescriptionCreate.as_view(model=Description))),
      url(r'^hypothesis/(?P<hypothesis_id>\d+)/add_summary/$', login_required(SummaryCreate.as_view(model=Summary))),
+     
+     
      
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
